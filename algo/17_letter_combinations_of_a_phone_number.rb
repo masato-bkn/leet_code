2691d66dd42e3927a77e503dbabb4a3faabf2c47
重田 将人 <masato.0928.j@gmail.com>
2020-09-23 23:51:11 +0900

    no.17解いた

--- algo/17_letter_combinations_of_a_phone_number.rb
+++ algo/17_letter_combinations_of_a_phone_number.rb
@@ -1,9 +1,37 @@
-# @param {String} digits
-# @return {String[]}
+# 17. Letter Combinations of a Phone Number
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+CHAR_HASH = {
+    "1": [],
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"]
+}
+
 def letter_combinations(digits)
-    # [ ] 数字と文字列のハッシュを作成sする
-    # [ ] 各桁に該当するバリューを取得
-    # [ ] 組み合わせを計算
+    return [] if digits.size == 0
+
+    result = []
+
+    # 組み合わせを計算
+    backtrack('', digits, result)
+    return result
+end
+
+# バックトレイス
+def backtrack(combination, next_digits, result)
+    if next_digits.size == 0
+        return result << combination
+    else
+        for target in CHAR_HASH[next_digits[0].to_sym] do
+            backtrack(combination + target, next_digits[1..-1], result)
+        end
+    end
 end
 
 p letter_combinations("23")
