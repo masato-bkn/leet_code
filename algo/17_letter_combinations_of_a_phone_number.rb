63a327d2c80798967ef7bd636fa7d5fcf75602a5
重田 将人 <masato.0928.j@gmail.com>
2020-09-23 20:50:05 +0900

    no.17の概形を作成

---
+++ algo/17_letter_combinations_of_a_phone_number.rb
@@ -0,0 +1,9 @@
+# @param {String} digits
+# @return {String[]}
+def letter_combinations(digits)
+    # [ ] 数字と文字列のハッシュを作成sする
+    # [ ] 各桁に該当するバリューを取得
+    # [ ] 組み合わせを計算
+end
+
+p letter_combinations("23")
